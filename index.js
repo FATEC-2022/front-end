92d84d8e07972be49cbf50c875eea7aa6357c9f2
JeffeVargasP <jeffevargas@proton.me>
2022-11-06 02:16:42 -0300

    UPDATE ON JAVASCRIPT

--- index.js
+++ index.js
@@ -1,70 +1,113 @@
-// DEFINING THE VARIABLES AND CONSTANTS
+// INSTANCIATING THE SPEECH RECOGNITION
+
+const recognition = new webkitSpeechRecognition();
+recognition.lang = "pt-BR";
+recognition.continuous = true;
+recognition.interimResults = false;
+
+// CREATING SPEECH SYNTHESIS FUNCTION
+
+function say(text) {
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.text = text;
+    utterance.lang = "pt-BR";
+    utterance.volume = 1;
+    utterance.rate = 1;
+    utterance.pitch = 1;
+    speechSynthesis.speak(utterance);
+}
+
+// CREATING SLEEP FUNCTION
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// DOM ELEMENTS
+
 const startBtn = document.querySelector('#start');
 const output = document.querySelector('#output');
 const assistent = document.querySelector('#assistent');
-// INSTANTIATING THE WEBKIT SPEECH RECOGNITION
-const recognition = new webkitSpeechRecognition();
 
-// DEFINING THE FUNCTION
+// ON START BUTTON CLICK
 function start() {
 
-    // DEFINING THE SPEECH RECOGNITION PROPERTIES
-    recognition.interimResults = false;
-    recognition.lang = "pt-BR";
-    recognition.continuous = false;
+    assistent.innerHTML = "Estou te ouvindo...";
+    say("Estou te ouvindo...");
+    // STARTING THE RECOGNITION
+    sleep(1200).then(() => {
+        recognition.start();
+    });
+}
 
-    // STARTING THE SPEECH RECOGNITION
-    recognition.start();
+// ON SPEECH RECOGNITION START
+recognition.onstart = function() {
 
-    // DEFINING THE SPEECH RECOGNITION EVENTS
-
-    // ON SPEECH RECOGNITION START
-    recognition.onstart = function() {
-        assistent.innerHTML = "Estou te ouvindo...";
-
+    recognition.onresult = function(event) {
+    
         // GETTING THE RECOGNITION RESULT
-        recognition.onresult = function(event) {
-            var last = event.results.length - 1;
-            var command = event
-                .results[last][0]
-                .transcript
-                .toLowerCase();
-            output.innerHTML = command;
+        let result = event.results[event.resultIndex][0].transcript;
+        result = result.toLowerCase();
+        // SETTING THE RECOGNITION RESULT TO THE OUTPUT
+        output.innerHTML = result;
 
-            // ON SPEECH RECOGNITION END
-            recognition.onend = function() {
-                assistent.innerHTML = "Não estou mais te ouvindo...";
-                console.log("Não estou mais te ouvindo...");
-            }
+        let callAssistent = []
+        let audio = document.createElement('audio');
 
-            var audio = document.createElement('audio');
-            var commandName = []
+        for (let i = 0; i < "alex".length; i++) {
+            callAssistent.push(result[i]);
+        }
 
-            // GETTING THE COMMAND NAME
+        // STRING TRATAMENT
+        callAssistent = callAssistent.join("");
+        result = result.replace(callAssistent, "");
 
-            for (var i = 0; i < 4; i++) {
-                commandName.push(command[i]);
-            }
-
-            commandName = commandName.join("");
-            command = command.replace(commandName, "");
-            if (commandName === "alex" && command === "") {
-                audio.src = "audio/diga.ogg";
-                audio.play();
-            } else if (commandName === "alex" && command.includes("horas")) {
-                audio.src = "audio/horas.ogg";
-                audio.play();
-            } else if (commandName === "alex" && command.includes("dia")) {
-                audio.src = "audio/bom dia.ogg";
-                audio.play();
-            } else if (commandName === "alex" && command.includes("tarde")) {
-                audio.src = "audio/boa tarde.ogg";
-                audio.play();
-            } else if (commandName === "alex" && command.includes("dólar")) {
-                
-            }
+        // COMMANDS LIST
+        if (callAssistent === "alex" && result === "") {
+            assistent.innerHTML = "Diga doutor, diga! 💬💬💬";
+            audio.src = "audio/diga.ogg";
+            audio.play();
+            sleep(5500).then(() => {
+                recognition.stop();
+            });
+        } else if (callAssistent === "alex" && result.includes("horas")) {
+            assistent.innerHTML = "Hora de fazer um arduino sem módulo 😬😬😬";
+            audio.src = "audio/horas.ogg";
+            audio.play();
+            sleep(5500).then(() => {
+                recognition.stop();
+            });
+        } else if (callAssistent === "alex" && result.includes("dia")) {
+            assistent.innerHTML = "Bom dia doutor! 😎😎😎";
+            audio.src = "audio/bom dia.ogg";
+            audio.play();
+            sleep(5500).then(() => {
+                recognition.stop();
+            });
+        } else if (callAssistent === "alex" && result.includes("tarde")) {
+            assistent.innerHTML = "Boa tarde mestre! 🔥🔥🔥";
+            audio.src = "audio/boa tarde.ogg";
+            audio.play();
+            sleep(5500).then(() => {
+                recognition.stop();
+            });
+        } else if (callAssistent === "alex" && result.includes("dólar")) {
+            
         }
     }
 }
 
+// ON SPEECH RECOGNITION END
+recognition.onend = function() {
+    assistent.innerHTML = "Não estou mais te ouvindo...";
+    say("Não estou mais te ouvindo...");
+}
+
+// ON SPEECH RECOGNITION ERROR
+recognition.onerror = function(event) {
+    assistent.innerHTML = "Ocorreu um erro: " + event.error;
+    say("Ocorreu um erro: " + event.error);
+}
+
+// STARTING THE RECOGNITION
 startBtn.addEventListener('click', start);
